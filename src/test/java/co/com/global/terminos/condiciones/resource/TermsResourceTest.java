b5c1e43aacdac9ea79f4942ae896505b6517f281
juan.salcedo <jsalcedo@proteccion.com.co>
2022-03-16 16:28:17 -0500

    Se agregaron los test del servicio rest aceptar terminos y condiciones

--- src/test/java/co/com/global/terminos/condiciones/resource/TermsResourceTest.java
+++ src/test/java/co/com/global/terminos/condiciones/resource/TermsResourceTest.java
@@ -1,7 +1,10 @@
 package co.com.global.terminos.condiciones.resource;
 
+import co.com.global.terminos.condiciones.dto.AcepTermsConditionsDTO;
 import co.com.global.terminos.condiciones.dto.TermsConditionsDTO;
+import co.com.global.terminos.condiciones.model.AcepTermsConditions;
 import co.com.global.terminos.condiciones.model.TermsConditions;
+import co.com.global.terminos.condiciones.model.repository.AcepTermsConditionsRepository;
 import co.com.global.terminos.condiciones.model.repository.TermsConditionsRepository;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
@@ -26,6 +29,9 @@ class TermsResourceTest {
 
     @InjectMock
     TermsConditionsRepository termsConditionsRepository;
+
+    @InjectMock
+    AcepTermsConditionsRepository acepTermsConditionsRepository;
 
     @BeforeEach
     void setUp(){
@@ -79,6 +85,42 @@ class TermsResourceTest {
                 .statusCode(200)
                 .body("texto", Is.is("test 3"))
                 .body("version",Is.is(3));
+
+    }
+
+    @Test
+    void testcreateAcepTermsConditionsCedula(){
+        LocalDate date = LocalDate.now();
+
+        AcepTermsConditionsDTO acepTermsDTO = new AcepTermsConditionsDTO("C","12-ll-34-5678",1,date);
+        AcepTermsConditions acepTerms = new AcepTermsConditions("C","12-ll-34-5678",1,date);
+
+        Mockito.when(acepTermsConditionsRepository.persist(acepTerms)).thenReturn(Uni.createFrom().item(acepTerms));
+
+        given().when().
+                header("Content-Type", "application/json")
+                .body(acepTermsDTO)
+                .post("/api/terms/agree")
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test
+    void testcreateAcepTermsConditionsPasaporte(){
+        LocalDate date = LocalDate.now();
+
+        AcepTermsConditionsDTO acepTermsDTO = new AcepTermsConditionsDTO("P","11-LL-55",1,date);
+        AcepTermsConditions acepTerms = new AcepTermsConditions("P","11-LL-55",1,date);
+
+        Mockito.when(acepTermsConditionsRepository.persist(acepTerms)).thenReturn(Uni.createFrom().item(acepTerms));
+
+        given().when().
+                header("Content-Type", "application/json")
+                .body(acepTermsDTO)
+                .post("/api/terms/agree")
+                .then()
+                .statusCode(200);
 
     }
 
