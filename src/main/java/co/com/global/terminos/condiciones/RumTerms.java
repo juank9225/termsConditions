4d8d4a1348a7d5b98a097e8af9e81de6564111e1
juan.salcedo <jsalcedo@proteccion.com.co>
2022-03-28 10:00:01 -0500

    Se crea la clase main para la aplicacion y se Refactoriza a la foma correcta de ejecucion

--- src/main/java/co/com/global/terminos/condiciones/RumTerms.java
+++ src/main/java/co/com/global/terminos/condiciones/RumTerms.java
@@ -1,17 +1,26 @@
 package co.com.global.terminos.condiciones;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class RumTerms {
 
     public static void main(String[] args) {
-        System.out.println(".................................... ");
-        System.out.println(".......Iniciando aplicacion......... ");
-        System.out.println(".......Terminos y condiciones....... ");
-        System.out.println(".................................... ");
+        Quarkus.run(TermsApp.class,args);
+    }
 
-        Quarkus.run(args);
+    public static class TermsApp implements QuarkusApplication{
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println(".................................... ");
+            System.out.println(".......Iniciando aplicacion......... ");
+            System.out.println(".......Terminos y condiciones....... ");
+            System.out.println(".................................... ");
+            Quarkus.waitForExit();//para terminar la aplicaccion
+            return 0;
+        }
     }
 }
